c023f0c7c3192b3f50c8266ebf64beb1b5c43d1f
Giovanni SIMON <giovanni.simon58@gmail.com>
2021-01-12 09:59:56 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Etudiant etudiant = new Etudiant("Simon","Nom",22);
+		etudiant.toString();
+
+	}
+
+}
